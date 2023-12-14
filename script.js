7e98495db11cb22a7391c6b019e13c0168ffc596
mikk27 <alarmikkudu@gmail.com>
2023-12-14 21:17:50 +0200

    minimal css added

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-const myLibrary = [
-  //insert data here
-];
-
-// DOM
-
-let newBookBtn = document.getElementById("new-book-button");
-
-newBookBtn.addEventListener("click", function () {
-  let newBookForm = document.getElementById("new-book-form");
-  newBookForm.style.display = "block";
-});
+const myLibrary = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -17,43 +6,100 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
-function render() {
-  let LibraryEl = document.querySelector("#library");
-  LibraryEl.innerHTML = "";
-  //
-  for (let i = 0; i < myLibrary.length; i++) {
-    let myBook = myLibrary[i];
-    let bookEl = document.createElement("div");
-    bookEl.setAttribute("class", "book-wrapper");
-    bookEl.innerHTML = `<p>Title: ${myBook.title}</p>
-    <p>Author: ${myBook.author}</p>`;
-    bookEl.innerHTML += `<p>Pages: ${myBook.pages}</p>`;
-    bookEl.innerHTML += `<p>Read: ${myBook.read}</p>`;
-    bookEl.innerHTML += `<button id="delete-${i}">Delete</button>`;
-    LibraryEl.appendChild(bookEl);
 
-    //delete and update
-    let deleteBtn = document.getElementById(`delete-${i}`);
-    let updateBtn = document.getElementById(`update-${i}`);
-    deleteBtn.addEventListener("click", () => {
-      myLibrary.splice(i, 1);
-      render();
-    });
-  }
-}
 // Define a function called addBooktoLibrary
 function addBooktoLibrary() {
+  // Get the value of the 'title' input field
   let title = document.getElementById("title").value;
+
+  // Get the value of the 'author' input field
   let author = document.getElementById("author").value;
+
+  // Get the value of the 'pages' input field
   let pages = document.getElementById("pages").value;
+
+  // Get the checked state of the 'read' checkbox
   let read = document.getElementById("read").checked;
+
+  // Create a new Book object with the retrieved values
   let newBook = new Book(title, author, pages, read);
+
+  // Log the new book object to the console
   console.log(newBook);
+
+  // Add the new book object to the 'myLibrary' array
   myLibrary.push(newBook);
+
+  // Render the updated library
   render();
 }
 
+// DOM
+let newBookBtn = document.getElementById("new-book-button");
 document.querySelector("#new-book-form").addEventListener("submit", (e) => {
   e.preventDefault();
   addBooktoLibrary();
 });
+
+newBookBtn.addEventListener("click", function () {
+  let newBookForm = document.getElementById("new-book-form");
+  newBookForm.style.display = "block";
+});
+
+function render() {
+  // Get the library element
+  const libraryEl = document.querySelector("#library");
+  // Clear the contents of the library element
+  libraryEl.innerHTML = "";
+
+  // Loop through each book in the myLibrary array
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = myLibrary[i];
+    // Create a new book element
+    const bookEl = document.createElement("div");
+    bookEl.setAttribute("class", "book-wrapper");
+
+    // Create a title element and set its text content
+    const titleEl = document.createElement("p");
+    titleEl.textContent = `Title: ${book.title}`;
+    // Append the title element to the book element
+    bookEl.appendChild(titleEl);
+
+    // Create an author element and set its text content
+    const authorEl = document.createElement("p");
+    authorEl.textContent = `Author: ${book.author}`;
+    // Append the author element to the book element
+    bookEl.appendChild(authorEl);
+
+    // Create a pages element and set its text content
+    const pagesEl = document.createElement("p");
+    pagesEl.textContent = `Pages: ${book.pages}`;
+    // Append the pages element to the book element
+    bookEl.appendChild(pagesEl);
+
+    // Create a read element and set its text content
+    const readEl = document.createElement("p");
+    readEl.textContent = `Read: ${book.read}`;
+    // Append the read element to the book element
+    bookEl.appendChild(readEl);
+
+    // Create a delete button element and set its text content
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    // Add a click event listener to the delete button
+    deleteBtn.addEventListener("click", () => {
+      // Remove the book from the myLibrary array at the current index
+      myLibrary.splice(i, 1);
+      // Render the library again
+      render();
+    });
+    // Append the delete button element to the book element
+    bookEl.appendChild(deleteBtn);
+
+    // Append the book element to the library element
+    libraryEl.appendChild(bookEl);
+  }
+
+  // Log a message indicating that rendering is complete
+  console.log("Rendering complete");
+}
